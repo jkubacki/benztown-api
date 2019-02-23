7adc35d2a6d86d414e68b6792155a000be3149c1
Jakub Kubacki <kubacki.jk@gmail.com>
2019-02-23 13:23:16 -0500

    Add path to file to element serializer

--- app/serializers/element_serializer.rb
+++ app/serializers/element_serializer.rb
@@ -2,5 +2,9 @@
 
 class ElementSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :tag_list, :file
+  attributes :name, :tag_list
+
+  attribute :file do |element|
+    Rails.application.routes.url_helpers.rails_blob_path(element.file, only_path: true)
+  end
 end
