746e6ff346365f5da0035cba08786492a4f76968
Jakub Kubacki <kubacki.jk@gmail.com>
2019-02-24 18:48:22 -0500

    Send email with link to the frontend on client invitation

---
+++ app/mailers/invitation_mailer.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class InvitationMailer < ApplicationMailer
+  layout "mailer"
+
+  def invite(client_id, mplc_id, invitation_url)
+    @client = Client.find(client_id)
+    @mplc = Mplc.find(mplc_id)
+    @invitation_url = invitation_url
+
+    mail(
+      to: @client.email,
+      from: @mplc.email,
+      subject: "#{@mplc.email} would like to share his library with you",
+    )
+  end
+end
--- spec/services/clients/invite_spec.rb
+++ spec/services/clients/invite_spec.rb
@@ -3,7 +3,11 @@
 require "rails_helper"
 
 RSpec.describe Clients::Invite do
-  subject { described_class.call(email: email) }
+  include Dry::Monads::Result::Mixin
+
+  subject { described_class.call(email: email, mplc: mplc) }
+
+  let(:mplc) { build(:mplc) }
 
   context "when email is not valid" do
     let(:email) { "notvalid" }
@@ -15,9 +19,44 @@ RSpec.describe Clients::Invite do
 
   context "when email is valid" do
     let(:email) { "user@example.com" }
+    let(:invite_mailer) { instance_double(ActionMailer::MessageDelivery, deliver_now: nil) }
 
-    it "returns success with invited client" do
-      expect(subject.value!.email).to eq email
+    before do
+      allow(Clients::InvitationUrl).to receive(:call).and_return(invitation_url_result)
+      allow(InvitationMailer).to receive(:invite).and_return(invite_mailer)
+    end
+
+    context "when generating invitation url is a failure" do
+      let(:invitation_url_result) { Failure(nil) }
+
+      it { is_expected.to be_failure }
+
+      it "doesn't create new user" do
+        expect { subject }.not_to change(Client, :count)
+      end
+    end
+
+    context "when generating invitation url is a success" do
+      let(:invitation_url) { "http://example.com?invitation_token=token" }
+      let(:invitation_url_result) { Success(invitation_url) }
+
+      it { is_expected.to be_success }
+
+      it "creates a new user" do
+        expect { subject }.to change(Client, :count).by(1)
+      end
+
+      it "returns success with invited client" do
+        expect(subject.value!.email).to eq email
+      end
+
+      it "sends invitation email" do
+        expect(InvitationMailer).to(
+          receive(:invite).with(anything, mplc.id, invitation_url)
+        ).once
+        expect(invite_mailer).to receive(:deliver_now).once
+        subject
+      end
     end
   end
 end
